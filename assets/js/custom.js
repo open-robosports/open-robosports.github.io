f03bb01d32e819b8c19f9c7d9c285183fa470b97
Alex Santos <alex.santos.ete@gmail.com>
2018-08-01 12:20:39 +0200

    Mejora localización de assets, añade posters de videos y play manual

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,17 +1,36 @@
 $(document).ready(function() {
+  // Aplica efecto fadeIn al cargar la web
   $('.showOnReady').each(function(i) {
     $(this).animate({'opacity': '1'}, 1500);
   });
+
+  // Deshabilita el autoplay para smartphones
+  if (/Android|webOS|iPhone|iPad|BlackBerry|Windows Phone|Opera Mini|IEMobile|Mobile/i.test(navigator.userAgent)){
+    $('video').removeAttr('autoplay');
+    $('video:not(.home-video)').attr('controls',"controls");
+
+  } else {
+    $('video').attr('autoplay');
+  }
+
+  // Aplica efecto fadeIn al hacer scroll o al cargar la web con un scroll
+  // predefinido
   $(window).scroll(function() {
-    /* Check the location of each desired element */
     $('.showOnScroll').each(function(i) {
       var bottom_of_object = $(this).position().top + $(this).outerHeight();
       var bottom_of_window = $(window).scrollTop() + $(window).height() - 250;
 
-      /* If the object is completely visible in the window, fade it it */
       if (bottom_of_window > bottom_of_object) {
         $(this).animate({'opacity': '1'}, 1500);
       }
     });
+  });
+  $('.showOnScroll').each(function(i) {
+    var bottom_of_object = $(this).position().top + $(this).outerHeight();
+    var bottom_of_window = $(window).scrollTop() + $(window).height() - 250;
+
+    if (bottom_of_window > bottom_of_object) {
+      $(this).animate({'opacity': '1'}, 1500);
+    }
   });
 });
