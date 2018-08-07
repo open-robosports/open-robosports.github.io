b64af7644cae663da094c95a6da3578fbe80b7fa
Alex Santos <alex.santos.ete@gmail.com>
2018-08-07 16:09:58 +0200

    Añade sección Eventos y Clasificación

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
   // Aplica efecto fadeIn al cargar la web
   $('.showOnReady').each(function(i) {
-    $(this).animate({'opacity': '1'}, 1500);
+    $(this).animate({opacity: '1'}, 1500);
   });
 
   // Deshabilita el autoplay para smartphones
-  if (/Android|webOS|iPhone|iPad|BlackBerry|Windows Phone|Opera Mini|IEMobile|Mobile/i.test(navigator.userAgent)){
+  if (/Android|webOS|iPhone|iPad|BlackBerry|Windows Phone|Opera Mini|IEMobile|Mobile/i
+          .test(navigator.userAgent)) {
     $('video').removeAttr('autoplay');
-    $('video:not(.home-video)').attr('controls',"controls");
-
+    $('video:not(.home-video)').attr('controls', 'controls');
   } else {
     $('video').attr('autoplay');
   }
@@ -21,7 +21,7 @@ $(document).ready(function() {
       var bottom_of_window = $(window).scrollTop() + $(window).height() - 250;
 
       if (bottom_of_window > bottom_of_object) {
-        $(this).animate({'opacity': '1'}, 1500);
+        $(this).animate({opacity: '1'}, 1500);
       }
     });
   });
@@ -30,7 +30,64 @@ $(document).ready(function() {
     var bottom_of_window = $(window).scrollTop() + $(window).height() - 250;
 
     if (bottom_of_window > bottom_of_object) {
-      $(this).animate({'opacity': '1'}, 1500);
+      $(this).animate({opacity: '1'}, 1500);
+    }
+  });
+
+  // Maneja los botones de "Ver más" de los ranking
+  $('.list-group-item.more').on('click', function(e) {
+    if ($(this).hasClass('open')) {
+      $('.more-list', $(this).closest('.ranking')).slideUp(250);
+      $(this).removeClass('open');
+      $(this).html(
+          '<i class="material-icons margin-right-0">arrow_drop_down</i> Ver más');
+    } else {
+      $('.more.open').trigger('click');
+      $('.more-list', $(this).closest('.ranking')).slideDown(250);
+      $(this).addClass('open');
+      $(this).html(
+          '<i class="material-icons margin-right-0">arrow_drop_up</i> Ver menos');
+    }
+  });
+
+  // Crea los fullCalendar
+  $('#competiciones-calendar').fullCalendar({
+    // put your options and callbacks here
+    height: 'auto',
+    header: {left: 'today prev,next', center: 'title', right: 'listYear'},
+    themeSystem: 'bootstrap4',
+    locale: 'es',
+    defaultView: 'listYear',
+    // events:"/competiciones/eventos.json",
+    events: function(start, end, timezone, callback) {
+      $.ajax({
+        url: '../competiciones/eventos.json',
+        type: 'GET',
+        datatype: 'json',
+        success: function(doc) {
+          console.log(doc);
+          var events = [];
+          $.each(doc, function(i, event) {
+            events.push({
+              title: event.title,
+              start: event.start,
+              end: event.end,
+              allDay: event.allDay,
+              url: event.url
+            });
+          });
+          callback(events);
+        },
+        error: function(err) {
+          alert('Error in fetching data');
+        }
+      });
+    },
+    eventClick: function(event) {
+      if (event.url) {
+        window.open(event.url, '_blank');
+        return false;
+      }
     }
   });
 });
